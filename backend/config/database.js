ccba9fa92daba49c0c091b98cfa1ac3715f3d6ee
VrushaliSetoo <vrushali@setoo.co>
2024-06-21 14:41:43 +0530

    database change

--- backend/config/database.js
+++ backend/config/database.js
@@ -7,15 +7,15 @@ LOCAL
 
 const path = require('path');
 
-module.exports = ({ env }) => ({
-  connection: {
-    client: 'sqlite',
-    connection: {
-      filename: path.join(__dirname, '..', env('DATABASE_FILENAME', '.tmp/data.db')),
-    },
-    useNullAsDefault: true,
-  },
-});
+// module.exports = ({ env }) => ({
+//   connection: {
+//     client: 'sqlite',
+//     connection: {
+//       filename: path.join(__dirname, '..', env('DATABASE_FILENAME', '.tmp/data.db')),
+//     },
+//     useNullAsDefault: true,
+//   },
+// });
 
 // module.exports = ({ env }) => ({
 //   connection: {
@@ -39,19 +39,19 @@ module.exports = ({ env }) => ({
 LIVE RENDER
 ...
 */
-// module.exports = ({ env }) => ({
-//   connection: {
-//     client: 'postgres',
-//     connection: {
-//       host: env('DATABASE_HOST', 'dpg-couugj0l6cac73bbitv0-a.oregon-postgres.render.com'),
-//       port: env.int('DATABASE_PORT', 5432),
-//       database: env('DATABASE_NAME', 'qualitiasoft'),
-//       user: env('DATABASE_USERNAME', 'qualitiasoft_user'),
-//       password: env('DATABASE_PASSWORD', 'C1IqRqlEp9nsWUEWiT2aJ2nikSfvYTZN'),
-//       schema: env('DATABASE_SCHEMA', 'public'), // Not required
-//       ssl: env.bool('DATABASE_SSL_SELF', true),
-//     },
-//     debug: false,
-//   },
-// });
+module.exports = ({ env }) => ({
+  connection: {
+    client: 'postgres',
+    connection: {
+      host: env('DATABASE_HOST', 'dpg-cplbgqmd3nmc73ctiq50-a.oregon-postgres.render.com'),
+      port: env.int('DATABASE_PORT', 5432),
+      database: env('DATABASE_NAME', 'metafirdemo'),
+      user: env('DATABASE_USERNAME', 'metafirdemo_user'),
+      password: env('DATABASE_PASSWORD', 'h1GtlR4ALHWGpb2le3puhanHo3PDOVG2'),
+      // schema: env('DATABASE_SCHEMA', 'public'), // Not required
+      ssl: env.bool('DATABASE_SSL_SELF', true),
+    },
+    debug: false,
+  },
+});
 
